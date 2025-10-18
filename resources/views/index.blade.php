445480d7c48191e8ff888e4b594b1f81db7f39ca
Sami-AL-njadat <sami.alnajadat@gmail.com>
2025-10-18 11:04:50 +0300

    sami update video

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -145,13 +145,24 @@
                 </div>
             </div>
 
-            <div class="video-wrap">
+            {{-- <div class="video-wrap">
                 <video autoplay="" loop="" muted="" class="custom-video" poster="">
                     <source src="video/pexels-2022395.mp4" type="video/mp4">
 
                     Your browser does not support the video tag.
                 </video>
+            </div> --}}
+
+
+
+            <div class="video-wrap">
+                <video autoplay muted loop playsinline webkit-playsinline preload="auto" class="custom-video"
+                    poster="">
+                    <source src="video/pexels-2022395.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
             </div>
+
         </section>
 
 
